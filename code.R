90bdef5c30f48a60a794a97fdb82f9649281483d
Owen Finn <owen.finn@drake.edu>
2021-04-19 08:57:50 -0500

    random forest

--- code.R
+++ code.R
@@ -1,25 +1,141 @@
+# Set Up ------------------------------------------------------------------
+library(ggplot2)
+library(randomForest)
+library(pROC)
+
 # start with a clean slate!
 rm(list = ls())
 
 # Set working directory
-setwd("~/DRAKE_DEV/SPRING_2021/STAT_190/final")
+setwd("~/DRAKE_DEV/SPRING_2021/STAT_190/newJob")
 
 # Read in data
 jobs <- read.csv("aug_train.csv")
 
+
+# Clean Data --------------------------------------------------------------
+# Currently any missing value is stored as a blank character, "", rather than NA
+# First for each variable we make the missing values = NA
+# Then I factor each categorical variable
 jobs$enrollee_id[jobs$enrollee_id == ""] <- NA
+
 jobs$city[jobs$city == ""] <- NA
+jobs$city <- as.factor(jobs$city)
+
 jobs$city_development_index[jobs$city_development_index == ""] <- NA
+
+
 jobs$gender[jobs$gender == ""] <- NA
+jobs$gender <- as.factor(jobs$gender)
+
 jobs$relevent_experience[jobs$relevent_experience == ""] <- NA
+jobs$relevent_experience <- as.factor(jobs$relevent_experience)
+
 jobs$enrolled_university[jobs$enrolled_university == ""] <- NA
+jobs$enrolled_university <- as.factor(jobs$enrolled_university)
+
 jobs$education_level[jobs$education_level == ""] <- NA
+jobs$education_level <- as.factor(jobs$education_level)
+
 jobs$major_discipline[jobs$major_discipline == ""] <- NA
+jobs$major_discipline <- as.factor(jobs$major_discipline)
+
 jobs$experience[jobs$experience == ""] <- NA
+jobs$experience <- as.factor(jobs$experience)
+
 jobs$company_size[jobs$company_size == ""] <- NA
+jobs$company_size <- as.factor(jobs$company_size)
+
 jobs$company_type[jobs$company_type == ""] <- NA
+jobs$company_type <- as.factor(jobs$company_type)
+
 jobs$last_new_job[jobs$last_new_job == ""] <- NA
+jobs$last_new_job <- as.factor(jobs$last_new_job)
+
 jobs$training_hours[jobs$training_hours == ""] <- NA
 jobs$target[jobs$target == ""] <- NA
+jobs$target <- as.factor(jobs$target)
 
-jobs2 <- jobs[complete.cases(jobs), ]
+# Use na.roughfix to fill in the missing values so there will not be any errors
+# when creating the random forest
+jobs <- na.roughfix(jobs)
+
+
+# Visualizations ----------------------------------------------------------
+ggplot(data = jobs) +
+  geom_point(aes(x = city_development_index, y = target))
+
+# Random Forest -----------------------------------------------------------
+# Set seed and create training and testing data sets
+RNGkind(sample.kind = "default")
+set.seed(190)
+train.idx <- sample(x = 1:nrow(jobs), size = floor(.8*nrow(jobs)))
+train.df <- jobs[train.idx,]
+test.df <- jobs[-train.idx,] 
+
+# Create baseline forest with high mtry, ntree
+# Using every variable except the ID and city. City is a factor with more than
+# 53 levels, so the random forest will not work.
+myforest <- randomForest(target ~ city_development_index + gender + relevent_experience +
+                           enrolled_university + education_level + major_discipline +
+                           experience + company_size + company_type + last_new_job + training_hours,
+                         data = train.df,
+                         ntree = 1000,
+                         mtry = 11, 
+                         importance = TRUE) 
+# Tuning forest
+mtry <- c(1:11) # Since we have 11 x variables
+keeps2 <- data.frame(m = rep(NA,length(mtry)),
+                     OOB_err_rate = rep(NA, length(mtry)))
+
+# Loop through forests for each mtry
+for (idx in 1:length(mtry)){
+  tempforest<- randomForest(target ~ city_development_index + gender + relevent_experience +
+                              enrolled_university + education_level + major_discipline +
+                              experience + company_size + company_type + last_new_job + training_hours,
+                            data = train.df,
+                            ntree = 1000,
+                            mtry = mtry[idx])
+  keeps2[idx, "m"] <- mtry[idx]
+  keeps2[idx,"OOB_err_rate"] <- mean(predict(tempforest)!= train.df$target)
+  print(mtry[idx])
+}
+qplot(m, OOB_err_rate, geom = c("line", "point"), data = keeps2) + 
+  theme_bw() + labs(x = "m (mtry) value", y = "OOB error rate")
+
+# Despite sqrt(11) = 3.3, the mtry value that minimized OOB error rate was
+# mtry = 2, OOB = 0.2210622
+
+# Create a final forest with mtry = 2
+final_forest <- randomForest(target ~ city_development_index + gender + relevent_experience +
+                           enrolled_university + education_level + major_discipline +
+                           experience + company_size + company_type + last_new_job + training_hours,
+                         data = train.df,
+                         ntree = 1000,
+                         mtry = 2, 
+                         importance = TRUE) 
+
+# Prediction
+test.df$forest_pred <- predict(final_forest, test.df, type = "class")
+
+# Prediction graph
+ggplot(data = test.df) +
+  geom_bar(aes(x = forest_pred, fill = target), position="fill") +
+  ggtitle("Relation of Predicted Target vs Actual Target") +
+  labs(x = "Predicted Overall Target",
+       y = "Proportional Amount") +
+  scale_fill_grey("Actual Target") + 
+  scale_colour_grey("Actual Target") + 
+  theme_bw()
+
+# ROC curve
+# 1 (looking for new job) is the value of interest
+pi_hat <- predict(final_forest, test.df, type = "prob")[, 1]
+
+rocCurve <- roc(response = test.df$target,
+                predictor = as.vector(pi_hat),
+                levels = c(0, 1))
+
+plot(rocCurve, print.auc = TRUE, print.thres = TRUE)
+
+varImpPlot(final_forest, type = 1)
